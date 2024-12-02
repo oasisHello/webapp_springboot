732aeb39de34d36dcbbb2050e65be2a187173420
oasishalo <gongstats@gmail.com>
2024-12-02 11:32:22 +0900

    initialization

---
+++ webspringboot/src/main/java/oasis/com/web/webspringboot/App.java
@@ -0,0 +1,10 @@
+package oasis.com.web.webspringboot;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
